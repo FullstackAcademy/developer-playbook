b0c5ea220f99954c11bff718ed79d6bdace163b1
JeremyRogersFSA <jeremy.rogers@fullstackacademy.com>
2023-12-06 14:26:50 -0700

    added color reset

--- wsl.sh
+++ wsl.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 ansible-galaxy collection install community.postgresql community.general
 echo ""
-echo -e "\033[0;31m Enter your sudo password again when prompted by BECOMES:"
+echo -e "\033[0;31m Enter your sudo password again when prompted by BECOMES:\033[0m"
 echo ""
 ansible-pull -K -C jeremyTest -U https://github.com/fullstackacademy/developer-playbook.git playbook.yml
 echo ""
