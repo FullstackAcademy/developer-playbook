51da10d979873de5c3ce011970d22f6bda3fe039
JeremyRogersFSA <jeremy.rogers@fullstackacademy.com>
2023-12-14 09:58:58 -0700

    various small fixes, added export HOMEBREW_PREFIX to zshrc, changed all appends to zshrc, needs final test and change of ansible-pull to main branch once tested thoroughly

--- macos.sh
+++ macos.sh
@@ -10,17 +10,17 @@ else
   HOMEBREW_PREFIX="/usr/local"
 fi
 # Next two lines - Run the homebrew "brew" command install
-(echo; echo 'eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"') >> $HOME/.zprofile
+(echo; echo 'eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"') >> $HOME/.zshrc
+echo "export HOMEBREW_PREFIX=$HOMEBREW_PREFIX" >> $HOME/.zshrc
 eval "($HOMEBREW_PREFIX/bin/brew shellenv)"
-source ~/.zprofile
+source $HOME/.zshrc
 brew update
 brew install ansible
 ansible-galaxy collection install community.postgresql community.general
 echo -e "\033[0;31m Enter your sudo password again when prompted by BECOMES:\033[0m"
 ansible-pull -K -C jeremyTest -U https://github.com/fullstackacademy/developer-playbook.git playbook.yml
 echo "Ansible setup complete, close your Terminal and open a new one..."
-source ~/.zshrc
-source ~/.zprofile
+source $HOME/.zshrc
 brew services start postgresql@15
 # brew services restart postgresql@15
 # Create a user through the 'postgres' psql user, with d(createdb) l(login) r(createrole) and s(superuser) permisisons - then create db for the login user
