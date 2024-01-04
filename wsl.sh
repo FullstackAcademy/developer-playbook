bf1715fa6e28aff506c8c9a595b14ae6c2c59e14
JeremyRogersFSA <jeremy.rogers@fullstackacademy.com>
2024-01-04 11:43:47 -0700

    add gh to install for Ubuntu if version >= 22. Added gh to macOS install using homebrew. Option for later use in machine setup if desired.

--- wsl.sh
+++ wsl.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Enter your sudo password:"
 sudo apt update
+sudo apt upgrade -y
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install -y gh
