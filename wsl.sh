13f660f34ecf10bb770e5a223e6f11479e5c9373
Jeremy Rogers <117938858+JeremyRogersFSA@users.noreply.github.com>
2024-01-04 13:12:43 -0700

    Update wsl.sh
    took out GH CLI from here

--- wsl.sh
+++ wsl.sh
@@ -4,7 +4,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y gh
 sudo apt install -y ansible
 ansible-galaxy collection install community.postgresql community.general
 echo ""
