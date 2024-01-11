d9e785b2fe5d47b4c73cfe2639549d2658a3bbf4
Mary Gordanier <mary.gordanier@fullstackacademy.com>
2024-01-11 14:41:38 -0500

    move common setup steps to machine-setup.sh and add more logs

--- machine-setup.sh
+++ machine-setup.sh
@@ -6,11 +6,13 @@
 # Initialize text format variables
 TEXT_RED="\033[0;31m"
 TEXT_RED_BOLD="\033[1;31m"
+TEXT_BLUE="\033[0;34m"
+TEXT_BLUE_BOLD="\033[1;34m"
 TEXT_CYAN="\033[0;36m"
 TEXT_RESET="\033[0m"
 #
 # Check OS and identify appropriate script
-# Fail fast if OS is not a supported
+# Fail if OS is not a supported
 MACHINE_SETUP_SCRIPT=""
 if [ "$(/usr/bin/uname)" == "Darwin" ]; then
     MACHINE_SETUP_SCRIPT="macos.sh"
@@ -20,8 +22,18 @@ else
     echo "$TEXT_RED_BOLD Operating system not recognized \n$TEXT_RED This development environment setup script can only be run on macOS or Linux \n For Windows, first install WSL and then run this script in WSL Ubuntu $TEXT_RESET"
     exit
 fi
-#
+## Run setup script for the identified OS with logging
 echo "$TEXT_CYAN START logging to ~/fsa-machine-setup-log.ansi \n$TEXT_RESET"
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/FullstackAcademy/developer-playbook/jeremyTest/$MACHINE_SETUP_SCRIPT)" 2>&1 | tee -a $HOME/fsa-machine-setup-log.ansi
 #
-echo "$TEXT_CYAN\n END logging \n*********************************************************************************************** $TEXT_RESET"
+# Return to complete universal setup steps
+echo "$TEXT_BLUE\n Run Ansible playbook... $TEXT_RESET"
+echo "$TEXT_BLUE_BOLD\n Enter your account password when prompted by BECOME $TEXT_RESET"
+echo "$TEXT_BLUE  If there is no output, your password was accepted and the playbook is running. $TEXT_RESET"
+echo "$TEXT_BLUE  Leave this terminal window open until all installations are complete. $TEXT_RESET"
+# *********** NOTE: remove "-C jeremyTest" before finalizing **********
+ansible-pull -K -C jeremyTest -U https://github.com/fullstackacademy/developer-playbook.git playbook.yml
+#
+echo "$TEXT_BLUE_BOLD END development environment setup. $TEXT_RESET"
+echo "\n Please close your terminal window and open a new one.\n"
+echo "$TEXT_CYAN\END logging \n*********************************************************************************************** $TEXT_RESET"
